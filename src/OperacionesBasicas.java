efb0641aaf1220a4b79091136aeb595df7722c12
David Bousquet <david@programaciondb.cl>
2019-09-25 15:23:28 -0300

    se crea clase operaciones basicas

---
+++ src/OperacionesBasicas.java
@@ -0,0 +1,19 @@
+public class OperacionesBasicas {
+
+    public int sumar(int a, int b){
+        return a+b;
+    }
+
+    public int restar(int a, int b){
+        return a-b;
+    }
+
+    public int multiplicar(int a, int b){
+        return a*b;
+    }
+
+    public int dividir(int a, int b){
+        return a/b;
+    }
+
+}
